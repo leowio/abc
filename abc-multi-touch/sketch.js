331867320401ddea3ce490ba9f992c4da8dd44f2
Leo Wang <meimprankster@gmail.com>
2026-01-21 12:29:25 +0800

    finish up class 1

--- abc-multi-touch/sketch.js
+++ abc-multi-touch/sketch.js
@@ -1,29 +1,27 @@
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
+  background(90, 200, 190);
 }
 
 function draw() {
-  background(90, 200, 190);
-  
   fill(0);
-  circle(width/2, height/2, 100);
-
 }
 
 // P5 touch events: https://p5js.org/reference/#Touch
 
 function touchStarted() {
-  console.log(touches);
+  for (let i = 0; i < touches.length; i++) {
+    let x = touches[i].x;
+    let y = touches[i].y;
+    circle(x, y, 100);
+  }
 }
 
-function touchMoved() {
-}
+function touchMoved() {}
 
-function touchEnded() {
-}
+function touchEnded() {}
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
